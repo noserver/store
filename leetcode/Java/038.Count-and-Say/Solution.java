f9f92f2609272143fdbdfd76cf6b8352ecfedec4
gudda <gudda@outlook.com>
2017-08-16 09:06:51 +0800

    直接循环遍历

--- leetcode/Java/038.Count-and-Say/Solution.java
+++ leetcode/Java/038.Count-and-Say/Solution.java
@@ -15,8 +15,25 @@ public class Solution {
                 }
                 read = read+Integer.toString(count)+String.valueOf(cur);
             }
-            
             return read;
         }
+    }
+    //
+    public String countAndSay2(int n){
+        String res = "1";
+        for(int i=1;i<n;i++){
+            String current = res;
+            res = "";
+            for(int j=0;j<current.length();j++){
+                char cur = current.charAt(j);
+                int  count = 1;
+                while(j+1<current.length()&&current.charAt(j+1)==cur){
+                    j++;
+                    count++;
+                }
+                res = res+Integer.toString(count)+String.valueOf(cur);
+            }
+        }
+        return res;
     }
 }
--- leetcode/Java/038.Count-and-Say/UseCase.java
+++ leetcode/Java/038.Count-and-Say/UseCase.java
@@ -2,5 +2,6 @@ public class UseCase{
     public static void main(String[] args){
         Solution sol = new Solution();
         System.out.println(sol.countAndSay(4));
+        System.out.println(sol.countAndSay2(4));
     }   
 }
